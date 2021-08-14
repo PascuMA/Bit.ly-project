72938578c10064f32966a9995a7d68950499649f
PascuMA <87703055+PascuMA@users.noreply.github.com>
2021-08-14 16:42:19 +0300

    Add files via upload
    Am modificat cateva lucruri la interfata web

---
+++ Visual Studio Code/Bit.ly-project/index.js
@@ -0,0 +1,29 @@
+
+
+const btn = document.getElementById("Copy");
+
+    btn.addEventListener("click", () => {
+      const text = document.getElementById("text").innerHTML;
+      const emt = document.createElement("input");
+      emt.setAttribute("value", text);
+      document.body.appendChild(emt);
+      emt.select();
+      document.execCommand("copy");
+      emt.parentNode.removeChild(emt);
+    });
+
+let link = "google.com"
+
+let clicked = false;
+function convertTo(){
+    if(!clicked){
+        clicked = true;
+        document.getElementById("text").innerHTML = link;
+    }
+}
+
+// $(document).ready(function(){
+//     $("input-btn").click(function(){
+//       $("text").toggleClass("show");
+//     });
+//   });
